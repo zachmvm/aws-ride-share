db50fd54cd119e009f580ffa3f01526fd3f12244
Zach Whitsett <111324096+zachmvm@users.noreply.github.com>
2025-02-24 18:20:10 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_IepfOVzqg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '69nj8gudn0quk0dvfj3ip9gk7s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '53sd3htdg73sqt45fn92emoe23', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_IepfOVzqg/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
