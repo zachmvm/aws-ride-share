86e72295a22fa9b7e3ad433d2a7c03895eb146d5
Zach Whitsett <111324096+zachmvm@users.noreply.github.com>
2025-03-04 15:03:46 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://es0fv8li64.execute-api.us-east-1.amazonaws.com/ride' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://es0fv8li64.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
